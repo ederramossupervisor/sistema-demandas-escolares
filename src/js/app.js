b59dba2d088d231c50f930ace4aa2a7a399d1f94
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2025-12-19 19:52:07 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3804,7 +3804,7 @@ async function salvarTokenFCMNoServidor(token) {
             };
             
             // Montar URL com TODOS os parâmetros
-            const url = `https://script.google.com/macros/s/AKfycbzVXxDt3DIjWfkirNtwBlKA2uQPyTVb_Te_3Xrn-V5anpwln4TcHF74vfvP_wy3ReAEMA/exec?callback=${callbackName}&acao=salvarSubscription&fcmToken=${encodeURIComponent(token)}&tipo=firebase&email=${encodeURIComponent(userEmail)}`;
+            const url = `https://script.google.com/macros/s/AKfycbzqO5qtOKSbpZ92mRL4UmMRvGE0ZQMhqtS7K5PGW5MU12Z4g1d7sBaDOMg0xrWaLWiLoQ/exec?callback=${callbackName}&acao=salvarSubscription&fcmToken=${encodeURIComponent(token)}&tipo=firebase&email=${encodeURIComponent(userEmail)}`;
             
             console.log("📡 Enviando para servidor:", url.substring(0, 100) + "...");
             
