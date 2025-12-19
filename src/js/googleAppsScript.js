0ef358335d0d8855d73db8d449d7d042452a178f
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2025-12-19 16:53:33 -0300

    Update googleAppsScript.js

--- src/js/googleAppsScript.js
+++ src/js/googleAppsScript.js
@@ -1,5 +1,5 @@
 // googleAppsScript.js - VERSÃO FINAL OTIMIZADA
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwPHLUnKJO-LWPcw4uSBbDXJz5ej2SyUcGkJtARQfPUDOPVQDVLM60Mqqu5U5xRS8OiqA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwfLZDqCBVfBUVnvOODB7Ws8bySdrGsZuxY6nusAtlv1_fD4qBCWprznPRD-V0KvjgUcg/exec';
 
 // ============================================
 // CONFIGURAÇÕES INTELIGENTES
